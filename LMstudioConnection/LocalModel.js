a9fa809e80e6e2c226fdedf61c721f6cc17549ef
JoseRaul42 <89216049+JoseRaul42@users.noreply.github.com>
2024-04-21 13:53:52 -0500

    speed and quality best with TheBloke/Llama-2-7B-Chat-GGUF/llama-2-7b-chat.Q4_K_M.gguf

--- LMstudioConnection/LocalModel.js
+++ LMstudioConnection/LocalModel.js
@@ -49,7 +49,7 @@ async function main() {
     const client = new LMStudioClient();
 
     // Load model
-    const model = await client.llm.load("TheBloke/Llama-2-7B-Chat-GGUF/llama-2-7b-chat.Q5_K_M.gguf", {noHup: true});
+    const model = await client.llm.load("TheBloke/Llama-2-7B-Chat-GGUF/llama-2-7b-chat.Q4_K_M.gguf", {noHup: true});
 
     // Read the JSON data
     const jsonData = JSON.parse(fs.readFileSync('C:\\Users\\Afro\\Projects\\JavascriptLMstudioTemplate\\LMstudioConnection\\output.json', 'utf8'));
